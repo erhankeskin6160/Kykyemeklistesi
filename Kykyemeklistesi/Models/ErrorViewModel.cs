aa931911a70f9ea5afa8e78f1711f943825d2e5a
Erhan Keskin <76396857+erhankeskin6160@users.noreply.github.com>
2024-11-17 16:25:48 +0300

    Add project files.

---
+++ Kykyemeklistesi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Kykyemeklistesi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
