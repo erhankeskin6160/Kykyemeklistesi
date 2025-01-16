3bb90d1777808d048c6b64b18069f7d85c031cd4
Erhan Keskin <76396857+erhankeskin6160@users.noreply.github.com>
2025-01-16 14:57:40 +0300

    .

--- Kykyemeklistesi/Models/User.cs
+++ Kykyemeklistesi/Models/User.cs
@@ -4,9 +4,29 @@
     {
         public int Id { get; set; }
 
+        public string? Name { get; set; }
+        public string ?Soyad { get; set; }
         public string Email{ get; set; }
         
         public string Password{ get; set; }
 
+
+        public string? University { get; set; }
+        public string? Department { get; set; }
+        public string? Faculty { get; set; }
+        public string? City { get; set; }
+
+        public string? Resim { get; set; } ="erkek.png";
+
+        public Gender? Gender { get; set; } =0;
+       
+
+
+
+    }
+    public enum Gender
+    {
+        Erkek,
+        Kadın
     }
 }
